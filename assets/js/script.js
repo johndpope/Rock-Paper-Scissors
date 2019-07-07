06c9476751d307e13ca6bf39e541b45dfd842a75
Nataliia Ryzha <ryzha89@gmail.com>
2019-07-06 22:17:32 -0700

    created basic index page

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC0kpxe63TnL4vvgCNno30pJ_W6wLn3FB0",
+    authDomain: "rock-paper-scissors-challange.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-challange.firebaseio.com",
+    projectId: "rock-paper-scissors-challange",
+    storageBucket: "",
+    messagingSenderId: "81088028553",
+    appId: "1:81088028553:web:48b026bd42e15df5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
